2bb8930cf88e96076374e7ccfc5bc3cb54a242ed
a-thread <aiden.threadgoode@gmail.com>
2020-11-07 00:44:13 -0500

    reformatting

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,36 +1,32 @@
 import React, { Component } from "react";
+import Box from "@material-ui/core/Box";
 // import { Link } from "react-router-dom";
 import "./style.css";
 
 class Contact extends Component {
     render() {
         return (
-            <main id="contact">
-                <div className="container">
-                    <div className="row">
-                        <h2 className="col-md-3 contact-title">
-                            Contact
-                </h2>
-                        <article className="col-md-9">
-                            <div className="contact-info">
-                                <a className="contact-item" href="https://github.com/a-thread" rel="noreferrer" target="_blank"><strong><i
-                                    className="fab fa-github"></i>
+            <Box id="contact">
+                <h2 className="col-md-3 contact-title">Contact</h2>
+                
+                <article className="col-md-9">
+                    <div className="contact-info">
+                        <a className="contact-item" href="https://github.com/a-thread" rel="noreferrer" target="_blank"><strong><i
+                            className="fab fa-github"></i>
                             Github:</strong> github.com/a-thread</a>
-                                <a className="contact-item" href="https://www.linkedin.com/in/a-thread" rel="noreferrer" target="_blank"><strong><i
-                                    className="fab fa-linkedin"></i> LinkedIn:</strong>
-                        linkedin.com/in/aiden-threadgoode/</a>
-                                <a className="contact-item" href="mailto:aiden.threadgoode@gmail.com"><strong><i
-                                    className="fas fa-paper-plane"></i>
-                            Email:</strong>
-                        aiden.threadgoode@gmail.com</a>
-                                <a className="contact-item" href="tel:2077491341"><i className="fas fa-phone"></i><strong>
-                                    Phone:</strong>
-                        207-749-1341</a>
-                            </div>
-                        </article>
+                        <a className="contact-item" href="https://www.linkedin.com/in/a-thread" rel="noreferrer" target="_blank"><strong><i
+                            className="fab fa-linkedin"></i> LinkedIn: </strong>
+                            linkedin.com/in/aiden-threadgoode/</a>
+                        <a className="contact-item" href="mailto:aiden.threadgoode@gmail.com"><strong><i
+                            className="fas fa-paper-plane"></i>
+                            Email: </strong>
+                            aiden.threadgoode@gmail.com</a>
+                        <a className="contact-item" href="tel:2077491341"><i className="fas fa-phone"></i><strong>
+                            Phone:</strong>
+                            207-749-1341</a>
                     </div>
-                </div>
-            </main>
+                </article>
+            </Box>
         )
     }
 }
--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,4 +1,6 @@
-import * as React from "react";
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import "./style.css";
 import headshot from "../../images/aiden.png";
 
@@ -7,23 +9,29 @@ import headshot from "../../images/aiden.png";
 class Intro extends React.Component {
     render() {
         return (
-            <div className="intro-background">
-                <div className="headshot-image col-md-6 col-sm-12">
-                    <img src={headshot} className="headshot-round img-fluid animate__animated animate__rollIn"
-                        alt="Headshot of Aiden" />
-                </div>
-                <div className="col-md-6 col-sm-12 intro">
-                    <h1 className="intro-title animate__animated animate__backInLeft">Hi, I'm <span className="green">Aiden.</span></h1>
-                    <h2 className="typing">
-                        <span className="typewrite" data-period="2000"
-                            data-type='[ "Full Stack Developer", "Graphic Designer", "Dog Dad", "Trans Rights Activist", "Communications Specialist"]'>
-                            <span className="wrap"> </span>
-                        </span>
-                    </h2>
-                    <a href="index.html#portfolio"><button type="button" className="btn btn-outline-light">Recent
-                Projects</button></a>
-                </div>
-            </div>
+            <>
+                <Box className="intro-background">
+                    <div className="container">
+                        <div className="headshot-image col-md-6 col-sm-12">
+                            <img src={headshot} className="headshot-round img-fluid animate__animated animate__rollIn"
+                                alt="Headshot of Aiden" />
+                        </div>
+                        <div className="col-md-6 col-sm-12 intro">
+                            <h1 className="intro-title animate__animated animate__backInLeft">Hi, I'm <span className="green">Aiden.</span></h1>
+                            <h2 className="typing">
+                                <span className="typewrite" data-period="2000"
+                                    data-type='[ "Full Stack Developer", "Graphic Designer", "Dog Dad", "Trans Rights Activist", "Communications Specialist"]'>
+                                    <span className="wrap"> </span>
+                                </span>
+                            </h2>
+                            <a href="#portfolio">
+                                <Button className="projects" variant="contained">Recent Projects</Button>
+                            </a>
+                        </div>
+                    </div>
+                </Box>
+                <div className="icon arrowContainer"><a href="#bio"><div className="arrow bounce"></div></a></div>
+            </>
         )
     }
 }
