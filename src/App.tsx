d3c17c8d282077c429cd8ee7932aa2ee6e898292
a-thread <aiden.threadgoode@gmail.com>
2020-11-08 07:10:17 -0500

    updating file paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Portfolio from "./pages/portfolio";
 import Resume from "./pages/resume";
+import { HOME_PATH, RESUME_PATH, PORTFOLIO_PATH} from "./pathNames";
 
 
 function App() {
   return (
     <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/resume" component={Resume} />
+        <Route exact path={HOME_PATH} component={Home} />
+        <Route exact path={PORTFOLIO_PATH} component={Portfolio} />
+        <Route exact path={RESUME_PATH} component={Resume} />
 
     </Router>
   );
