ad8d196c9e2f73ae61534d32547a51c96cf6351f
a-thread <aiden.threadgoode@gmail.com>
2020-11-08 07:57:07 -0500

    updating path name

--- src/pathNames.tsx
+++ src/pathNames.tsx
@@ -1,5 +1,5 @@
 
-const base = process.env.NODE_ENV === "development" ? "/react-ts-portfolio" : "";
+const base = process.env.NODE_ENV === "development" ? "/aiden-react-ts-portfolio" : "";
 export const HOME_PATH = base + "/";
 export const PORTFOLIO_PATH = base + "/portfolio";
 export const RESUME_PATH = base + "/resume";
