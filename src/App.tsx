9b0318308b03d3c2c352a250f43564a6785da6ae
a-thread <aiden.threadgoode@gmail.com>
2020-11-08 07:53:55 -0500

    adding paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Portfolio from "./pages/portfolio";
 import Resume from "./pages/resume";
-import { HOME_PATH, RESUME_PATH, PORTFOLIO_PATH} from "./pathNames";
+import { HOME_PATH, RESUME_PATH, PORTFOLIO_PATH } from "./pathNames";
 
 
 function App() {
   return (
     <Router>
+      <Switch>
         <Route exact path={HOME_PATH} component={Home} />
         <Route exact path={PORTFOLIO_PATH} component={Portfolio} />
         <Route exact path={RESUME_PATH} component={Resume} />
+        <Route component={Home} />
+
+      </Switch>
 
     </Router>
   );
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { HOME_PATH, RESUME_PATH, PORTFOLIO_PATH } from "../../pathNames";
 import "./style.css";
 
 class Nav extends Component {
@@ -10,9 +11,9 @@ class Nav extends Component {
                     menu <i className="fa fa-bars"></i>
                 </div>
                 <ul>
-                    <li><a className="active" href="/">Home</a></li>
-                    <li><a href="/portfolio">Projects</a></li>
-                    <li><a href="/resume">Resume</a></li>
+                    <li><a className="active" href={HOME_PATH}>Home</a></li>
+                    <li><a href={PORTFOLIO_PATH}>Projects</a></li>
+                    <li><a href={RESUME_PATH}>Resume</a></li>
                     <li><a className="close-nav"><i className="fa fa-times"> <span className="visible-xs"></span></i></a></li>
                 </ul>
             </nav>
