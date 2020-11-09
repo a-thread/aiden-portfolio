d5bc53cbd4ec6470c84dedcd9671c09f5d5e8543
a-thread <aiden.threadgoode@gmail.com>
2020-11-08 19:03:31 -0500

    typewrite formatting

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -3,12 +3,14 @@ import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import { PORTFOLIO_PATH } from "../../utils/pathNames";
+import Typical from 'react-typical';
 import headshot from "../../images/aiden.png";
 import "./style.css";
 
 
 
 class Intro extends React.Component {
+
     render() {
         return (
             <>
@@ -20,12 +22,12 @@ class Intro extends React.Component {
                         </div>
                         <div className="col-md-8 col-sm-12 intro">
                             <h1 className="intro-title animate__animated animate__backInLeft">Hi, I'm <span className="green">Aiden.</span></h1>
-                            <h2 className="typing">
-                                <span className="typewrite" data-period="2000"
-                                    data-type='[ "Full Stack Developer", "Graphic Designer", "Dog Dad", "Trans Rights Activist", "Communications Specialist"]'>
-                                    <span className="wrap"> </span>
-                                </span>
-                            </h2>
+                            <Typical
+                                steps={[ 1000, 'Full Stack Developer', 2000, 'Graphic Designer', 1000, 'Dog Dad', 1000, 'Trans Rights Activist']}
+                                loop={Infinity}
+                                wrapper="h2"
+                                className="typewrite"
+                            />
                             <Link to={PORTFOLIO_PATH}>
                                 <Button className="projects" variant="contained" aria-label="projects">Recent Projects</Button>
                             </Link>
