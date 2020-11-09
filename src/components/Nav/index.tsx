bc8e6a2435b1577fb0136f1709cea7d2569fc62c
a-thread <aiden.threadgoode@gmail.com>
2020-11-08 19:03:53 -0500

    MVP nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -8,14 +8,10 @@ class Nav extends Component {
     render() {
             return(
             <nav role = 'navigation' >
-                <div className="toggle">
-                        menu <i className="fa fa-bars"></i>
-                </div>
                 <ul>
                     <li><Link to={HOME_PATH} className="active">Home</Link></li>
                     <li><Link to={PORTFOLIO_PATH}>Projects</Link></li>
                     <li><Link to={RESUME_PATH}>Resume</Link></li>
-                    <li><a className="close-nav"><i className="fa fa-times"> <span className="visible-xs"></span></i></a></li>
                 </ul>
             </nav>
         )
@@ -23,3 +19,16 @@ class Nav extends Component {
 }
 
 export default Nav;
+
+
+{/* <nav role = 'navigation' >
+<div className="toggle">
+        menu <i className="fa fa-bars"></i>
+</div>
+<ul>
+    <li><Link to={HOME_PATH} className="active">Home</Link></li>
+    <li><Link to={PORTFOLIO_PATH}>Projects</Link></li>
+    <li><Link to={RESUME_PATH}>Resume</Link></li>
+    <li><a className="close-nav"><i className="fa fa-times"> <span className="visible-xs"></span></i></a></li>
+</ul>
+</nav> */}
