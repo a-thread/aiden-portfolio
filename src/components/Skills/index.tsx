42b357b631b4e51d8e968d0d2d9aa20793da6415
a-thread <aiden.threadgoode@gmail.com>
2020-12-13 00:47:42 -0500

    adding skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -11,24 +11,24 @@ class Skills extends Component {
                 <div className="skillsSection">
                     <div>
                         <span><i className="fas fa-laptop-code"></i></span>
-                        <h2>Front End</h2>
-                        <p>Angular, React, HTML5, CSS, jQuery, Bootstrap, Material</p>
+                        <h4>Front End</h4>
+                        <p>Angular, React, HTML5, CSS, SCSS, jQuery, Bootstrap, Material</p>
                     </div>
                     <div>
                         <span><i className="fas fa-terminal"></i></span>
-                        <h2>Back End</h2>
+                        <h4>Back End</h4>
                         <p>Typescript, Javascript, NestJs, Mongoose, Express, NodeJs, SQL, NoSQL, MongoDB</p>
                     </div>
                 </div>
                 <div className="skillsSection">
                     <div>
                         <span className="blue"><i className="fas fa-code-branch"></i></span>
-                        <h2>Tools</h2>
-                        <p>Github, VSCode, Git, Jira, Hubspot, Slack</p>
+                        <h4>Tools</h4>
+                        <p>Git, Github, Jira, Confluence, VSCode, Hubspot, Slack</p>
                     </div>
                     <div className="soft">
                         <span className="blue"><i className="fas fa-users-cog"></i></span>
-                        <h2>Soft Skills</h2>
+                        <h4>Soft Skills</h4>
                         <p>Critical Thinking, Communication, Resourcefulness, Organization, Flexibility</p>
                     </div>
                 </div>
